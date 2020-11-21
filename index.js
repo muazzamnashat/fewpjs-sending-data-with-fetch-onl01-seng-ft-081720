ffe3d41ac8f66c95b9ded56563c1e3bc9ed27848
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-21 05:49:49 -0500

    commit tree 9db3aeefd178dc83060ef4cb14f39a6782b9643c

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData ( name, email){
+    let data = {name: name, email:email};
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify(data)
+    };
+
+    return fetch("http://localhost:3000/users",configObj)
+        .then(response => response.json())
+        .then(object => document.body.innerHTML += object.id)
+        .catch(error => document.body.innerHTML += error.message)
+}
+submitData("Nashat","nashatchy@gmail.com");
